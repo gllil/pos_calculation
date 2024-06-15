2413c888ca0422ced0569a64c6d772dd2135348b
gllil <garyjllil@gmail.com>
2024-06-15 00:11:03 -0400

    removed old files and old packages

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.json());
-
-const salesCalculationsRouter = require("./routes/salesCalculations");
-app.use("/calculations", salesCalculationsRouter);
-
-app.listen(3000, () => console.log("server 3000 has started"));
